8e97f88ab2603243cec49ba6cb3f66b0af3d1b4c
vicky vishwas <technogeniussky@gmail.com>
2023-10-12 19:17:27 +0530

    nice

---
+++ app/src/main/java/com/codenation/myapplication55/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codenation.myapplication55;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
